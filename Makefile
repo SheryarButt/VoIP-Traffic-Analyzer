297acd7f8d57f459e981694d2bc29698f78df7c2
SheryarButt <m.sheryarbutt@gmail.com>
2022-04-24 17:44:52 +0500

    Added build tags for Hyperscan and added sip classifier in main module.

--- Makefile
+++ Makefile
@@ -3,7 +3,11 @@
 # license that can be found in the LICENSE file.
 
 PATH_TO_MK = /root/nff-go/mk
-IMAGENAME = nff-go-ims-main
+IMAGENAME = VoIP-Analyzer-main
 EXECUTABLES = VoIP-Analyzer
+
+export GO_BUILD_TAGS += -tags hyperscan_v4
+gohs:
+	go get -tags hyperscan_v4 -v github.com/flier/gohs/hyperscan
 
 include $(PATH_TO_MK)/leaf.mk
--- VoIP-Analyzer.go
+++ VoIP-Analyzer.go
@@ -1,16 +1,47 @@
 package main
 
+/*
+ * FILE_NAME:
+ 		VoIP-Analyzer.go
+ * MODULE:
+ 		Main
+ * DESCRIPTION:
+ 		This file contains the Main Module for VoIP-Traffic-Analyzer.
+ * Expected Input:
+		General IP Reassembled IMS traffic.
+ * Expected Output:
+		General IP Reassembled IMS traffic after analysis.
+*/
+
 import (
 	"VoIP-Analyzer/common"
 	"VoIP-Analyzer/protocolIdentifier"
+	"VoIP-Analyzer/sipClassifier"
 
 	"github.com/intel-go/nff-go/flow"
 )
 
-// Main function for constructing packet processing graph.
+func init() {
+	sipClassifier.PatternSetup()
+}
+
+/*
+ *	FUNCTION_NAME :
+ 		main
+ *	FUNCTION TYPE :
+ 		Main Function
+ *	ARGS :
+ 		None
+ *	RETURN TYPE :
+ 		None
+ * 	DESCRIPTION :
+		Main function for constructing packet processing graph.
+*/
+
 func main() {
 
 	common.SplashScreen()
+	var Hs_Obj = new(sipClassifier.HS)
 
 	config := flow.Config{
 		CPUList:     "",
@@ -21,11 +52,17 @@ func main() {
 	flowInput, err := flow.SetReceiver(0)
 	flow.CheckFatal(err)
 
-	flowPBF, _ := flow.SetSplitter(flowInput, protocolIdentifier.Identify, 3, nil)
+	flowPI, _ := flow.SetSplitter(flowInput, protocolIdentifier.Identify, 3, nil)
+	flow.CheckFatal(flow.SetSender(flowPI[protocolIdentifier.RTP], 0))
 
-	flow.CheckFatal(flow.SetSender(flowPBF[protocolIdentifier.NON_TCP], 0))
-	flow.CheckFatal(flow.SetSender(flowPBF[protocolIdentifier.TCP], 0))
-	flow.CheckFatal(flow.SetSender(flowPBF[protocolIdentifier.RTP], 0))
+	flowSIPTcp, _ := flow.SetSeparator(flowPI[protocolIdentifier.TCP], sipClassifier.Classify, Hs_Obj)
+	flow.CheckFatal(flow.SetSender(flowPI[protocolIdentifier.TCP], 0))
+
+	flowSIPUdp, _ := flow.SetSeparator(flowPI[protocolIdentifier.NON_TCP], sipClassifier.Classify, Hs_Obj)
+	flow.CheckFatal(flow.SetSender(flowPI[protocolIdentifier.NON_TCP], 0))
+
+	flow.CheckFatal(flow.SetSender(flowSIPTcp, 0))
+	flow.CheckFatal(flow.SetSender(flowSIPUdp, 0))
 
 	flow.SystemStart()
 
